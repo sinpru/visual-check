a41347f7b9de15465f3c63ce39dbface2d492e96
Binhs <nguyentranthanhbinh123@gmail.com>
2026-03-31 15:31:44 +0700

    feat(core): .env path fix

--- dashboard/next.config.ts
+++ dashboard/next.config.ts
@@ -1,4 +1,8 @@
 import type { NextConfig } from "next";
+import dotenv from "dotenv";
+import path from "node:path";
+
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
 const nextConfig: NextConfig = {
   /* config options here */
